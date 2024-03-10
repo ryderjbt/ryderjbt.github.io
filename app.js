71b843f9c27bc710a4041e09b2508989aec0c17c
ryder.jbt <ryder.jbt@outlook.com>
2024-03-10 17:25:50 +0000

    mum update

--- app.js
+++ app.js
@@ -6,99 +6,55 @@ function alertButton()
 function catButton() 
 {
     var theImages = [{
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/2cfd6392-a979-4427-84e6-fe0bd724f521.jpg",
+        src: "https://ryderjbt.github.io/Images/Mum/3aaf537a-3afb-4935-b8aa-941b4547dad1.jpg",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/4DB4F81A-2C55-4FC5-BE52-67C0574C1327.jpg",
+        src: "https://ryderjbt.github.io/Images/Mum/8aa12044-5284-4d63-ac87-6ebc19b91499.jpg",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/be3ac2e0-94d0-4b3f-bc1b-67c88e67868c.jpg",
+        src: "https://ryderjbt.github.io/Images/Mum/43237169-d572-493a-bd22-51535323f524.jpg",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_0021.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/a754b7ec-469f-4685-b5b1-05651f0ae9ca.jpg",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_0034.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/c548d2a3-0bd0-4f93-a6d4-096e4c458274.jpg",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_0095.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_0314.PNG",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_0275.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_0700.HEIC",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_0278.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_0704.HEIC",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_1013.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_1308.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_1036.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_1315.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_1068.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_1326.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_1170.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_2297.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_1171.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_3967.JPG",
     }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_1196.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_5197.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4487.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_6138.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4488.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_8518.HEIC",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4489.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_8520.HEIC",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4490.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_1317.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4491.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_1330.PNG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4492.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_3524.HEIC",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4708.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_3969.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4795.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_5198.JPG",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4806.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_8517.HEIC",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4815.jpeg",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_8519.HEIC",
     }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4827.jpeg",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4839.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4887.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4889.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4890.jpeg",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4891.jpeg",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4919.jpeg",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_4942.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5003.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5570.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5571.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5693.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5831.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5835.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5836.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5838.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_5949.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_6006.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_6134.JPG",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_6309.jpeg",
-    }, { 
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_6310.jpeg",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_7089.jpeg",
-    }, {
-        src: "https://ryderjbt.github.io/Images/Catsite/Lillith/IMG_7290.JPG",
+        src: "https://ryderjbt.github.io/Images/Mum/IMG_8521.HEIC",
     }];
    
    var preBuffer = [];
